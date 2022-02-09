f6b28f8a2bddcc4b667411bbcb034c033b5d653e
Igor-Yarakhovich <igtopor@yandex.ru>
2022-02-09 09:53:58 +0300

    add styles

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -11,12 +11,54 @@ import settingsIcon from '../../assets/icons/settings.png'
 export const Navbar = () => {
     return (
         <nav className={n.nav}>
-            <div className={n.item}><NavLink to="/Profile" activeClassName={n.activeLink}><img className={n.icon} src={profileIcon} alt=''/>Profile</NavLink></div>
-            <div className={n.item}><NavLink to="/Message" activeClassName={n.activeLink}><img className={n.icon} src={messageIcon} alt=''/>Message</NavLink></div>
-            <div className={n.item}><NavLink to="/Users" activeClassName={n.activeLink}><img className={n.icon} src={usersIcon} alt=''/>Users</NavLink></div>
-            <div className={n.item}><NavLink to="/News" activeClassName={n.activeLink}><img className={n.icon} src={newsIcon} alt=''/>News</NavLink></div>
-            <div className={n.item}><NavLink to="/Music" activeClassName={n.activeLink}><img className={n.icon} src={musicIcon} alt=''/>Music</NavLink></div>
-            <div className={n.item}><NavLink to="/Settings" activeClassName={n.activeLink}><img className={n.icon} src={settingsIcon} alt=''/>Settings</NavLink></div>
+            <div className={n.item}>
+                <NavLink to="/Profile" activeClassName={n.activeLink}>
+                    <div className={n.itemContent}>
+                        <div><img className={n.icon} src={profileIcon} alt=''/></div>
+                        <div className={n.itemText}>Profile</div>
+                    </div>
+                </NavLink>
+            </div>
+            <div className={n.item}>
+                <NavLink to="/Message" activeClassName={n.activeLink}>
+                    <div className={n.itemContent}>
+                        <div><img className={n.icon} src={messageIcon} alt=''/></div>
+                        <div className={n.itemText}>Message</div>
+                    </div>
+                </NavLink>
+            </div>
+            <div className={n.item}>
+                <NavLink to="/Users" activeClassName={n.activeLink}>
+                    <div className={n.itemContent}>
+                        <div><img className={n.icon} src={usersIcon} alt=''/></div>
+                        <div className={n.itemText}>Users</div>
+                    </div>
+                </NavLink>
+            </div>
+            <div className={n.item}>
+                <NavLink to="/News" activeClassName={n.activeLink}>
+                    <div className={n.itemContent}>
+                        <div><img className={n.icon} src={newsIcon} alt=''/></div>
+                        <div className={n.itemText}>News</div>
+                    </div>
+                </NavLink>
+            </div>
+            <div className={n.item}>
+                <NavLink to="/Music" activeClassName={n.activeLink}>
+                    <div className={n.itemContent}>
+                        <div><img className={n.icon} src={musicIcon} alt=''/></div>
+                        <div className={n.itemText}>Music</div>
+                    </div>
+                </NavLink>
+            </div>
+            <div className={n.item}>
+                <NavLink to="/Settings" activeClassName={n.activeLink}>
+                    <div className={n.itemContent}>
+                        <div><img className={n.icon} src={settingsIcon} alt=''/></div>
+                        <div className={n.itemText}>Settings</div>
+                    </div>
+                </NavLink>
+            </div>
         </nav>
     )
 }
